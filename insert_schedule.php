114f3f83d53f9c6c1e46e149fccf51995364e062
“Alnada“ <aalemadi@andrew.cmu.edu>
2021-11-23 23:34:26 +0300

    23/11/2021

--- insert_schedule.php
+++ insert_schedule.php
@@ -56,7 +56,7 @@ $_SESSION['students_rfid'];
                     array_push($classDuration,$_POST[$duration]);
                     $current = $x-1;
                     $sql_get_image_id = "SELECT Image_id FROM TEACHER_CLASS_IMAGES WHERE Subject_name = '$classes[$current]' AND TeacherUsername = '$_SESSION[username1]'";
-                    array_push($classes,$_POST[$id]);
+                    // array_push($classes,$_POST[$id]);
                 // echo $sql_get_image_id;
                 // echo '<br>';
                 $result_get_image_id = $conn->query($sql_get_image_id);
@@ -67,14 +67,14 @@ $_SESSION['students_rfid'];
                 }
             }
                 
-                
                 // echo '<br>';
             }
             echo '<br>';
-            // print_r($classes_imageID);
             $sql_insert = "INSERT INTO SCHEDULE (Schedule_name,Created_by,show_text,start_time,end_time,class1,class2,class3,class4,class5,class6,class7,class8,class9,class10,class_duration1,class_duration2,class_duration3,class_duration4,class_duration5,class_duration6,class_duration7,class_duration8,class_duration9,class_duration10) VALUES ('$_POST[schedule_name]', '$_SESSION[username1]', $showtext, '$_POST[start_time]', '$_POST[end_time]', $classes_imageID[0], $classes_imageID[1],$classes_imageID[2],$classes_imageID[3],$classes_imageID[4],$classes_imageID[5],$classes_imageID[6],$classes_imageID[7],$classes_imageID[8],$classes_imageID[9],$classDuration[0],$classDuration[1],$classDuration[2],$classDuration[3],$classDuration[4],$classDuration[5],$classDuration[6],$classDuration[7],$classDuration[8],$classDuration[9])";
+            // echo $sql_insert;
             if ($conn->query($sql_insert) === TRUE) {
                 $_SESSION['last_id'] = $conn->insert_id;
+                $last_id = $_SESSION['last_id'];
             echo '<nav class="navbar navbar-expand-sm bg-light navbar-light">
         <div class="container-fluid">
             
@@ -120,7 +120,7 @@ $_SESSION['students_rfid'];
           echo '<input type="submit" value="Send Scheduel">
                 </form>';
             } else {
-                // header("Location: createschedule.php");
+                header("Location: createschedule.php");
             }
             
             
